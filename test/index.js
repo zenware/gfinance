891ef91267a2f898a3f08991c833ef642bc0c9b9
jay.m.looney@gmail.com <jay.m.looney@gmail.com>
2016-02-14 18:35:26 -0600

    added functionality and a test

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 import assert from 'assert';
 import gfinance from '../lib';
 
-describe('gfinance', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+describe('gfinance', () => {
+  it('this function has output', () => {
+    gfinance.get(['GOOG'], assert);
   });
 });
