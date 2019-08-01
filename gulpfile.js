00959ff886cbf178f885399f196a3373c5ffbf97
Jay Looney <jay.m.looney@gmail.com>
2019-08-01 09:41:13 -0500

    Remove istanbul and isparta calls from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,11 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const excludeGitignore = require('gulp-exclude-gitignore');
 const mocha = require('gulp-mocha');
-const istanbul = require('gulp-istanbul');
 const plumber = require('gulp-plumber');
 const coveralls = require('gulp-coveralls');
 const babel = require('gulp-babel');
 const del = require('del');
-const isparta = require('isparta');
+import run from "gulp-run-command";
 
 // Initialize the babel transpiler so ES2015 files gets compiled
 // when they're loaded
@@ -24,35 +23,12 @@ gulp.task('static',
     .pipe(eslint.failAfterError())
 );
 
-gulp.task('pre-test',
-  () => gulp.src('lib/**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(istanbul({
-      includeUntested: true,
-      instrumenter: isparta.Instrumenter
-    }))
-    .pipe(istanbul.hookRequire())
-);
-
-gulp.task('test', ['pre-test'], cb => {
-  let mochaErr;
-
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
-    .on('error', err => {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', () => {
-      cb(mochaErr);
-    });
-});
-
 gulp.task('watch', () => {
   gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
 });
 
+gulp.task('test-coverage', [run('npm i nyc')])
+gulp.task('test', ['test-coverage'])
 gulp.task('coveralls', ['test'], () => {
   if (!process.env.CI) {
     return undefined;
