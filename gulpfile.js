b687ec3d64a6a4fd19cd9bb87456a05f846126ff
Jay Looney <jay.m.looney@gmail.com>
2019-08-12 09:45:29 -0500

    Upgrade to gulpv4 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -14,28 +14,28 @@ const run = require('gulp-run-command').default
 // when they're loaded
 require('babel-core/register');
 
-gulp.task('static',
+gulp.task('static', gulp.series(
   () => gulp.src('**/*.js')
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
-);
+));
 
 gulp.task('watch', () => {
   gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
 });
 
-gulp.task('test-coverage', [run('npm i nyc')])
-gulp.task('test', ['test-coverage'])
+gulp.task('test-coverage', gulp.series(run('npm i nyc')));
+gulp.task('test', gulp.series('test-coverage'))
 
-gulp.task('babel', ['clean'],
+gulp.task('babel', gulp.series('clean',
   () => gulp.src('lib/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'))
-);
+));
 
-gulp.task('clean', () => del('dist'));
+gulp.task('clean', gulp.series(() => del('dist')));
 
-gulp.task('prepublish', ['babel']);
-gulp.task('default', ['static', 'test']);
+gulp.task('prepublish', gulp.series('babel'));
+gulp.task('default', gulp.parallel('static', 'test'));
