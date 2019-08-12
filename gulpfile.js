5cda1270c3c4ca13c7f2ac0636b742accd5a013a
Jay Looney <jay.m.looney@gmail.com>
2019-08-12 15:10:06 -0500

    new gulpfile format

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,66 @@
 'use strict';
 
-const path = require('path');
-const gulp = require('gulp');
+const { watch, series, parallel, src, dest } = require('gulp');
 const eslint = require('gulp-eslint');
 const excludeGitignore = require('gulp-exclude-gitignore');
 const mocha = require('gulp-mocha');
 const plumber = require('gulp-plumber');
 const babel = require('gulp-babel');
 const del = require('del');
-const run = require('gulp-run-command').default
+const { exec } = require('child_process');
 
-// Initialize the babel transpiler so ES2015 files gets compiled
-// when they're loaded
-require('babel-core/register');
-
-gulp.task('static', gulp.series(
-  () => gulp.src('**/*.js')
+function staticFiles() {
+  return src('**/*.js')
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
-));
+}
 
-gulp.task('watch', () => {
-  gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
-});
+//TODO: Update this section to support watchers
+//gulp.task('watch', () => {
+//  gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
+//});
+//
 
-gulp.task('test-coverage', gulp.series(run('npm i nyc')));
-gulp.task('test', gulp.series('test-coverage'))
+function testCoverage() {
+  return exec('npm i nyc');
+}
 
-gulp.task('babel', gulp.series('clean',
-  () => gulp.src('lib/**/*.js')
+function jsTranspile() {
+  // With Babel
+  return src('lib/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'))
-));
+}
 
-gulp.task('clean', gulp.series(() => del('dist')));
+function clean() {
+  return del('dist');
+}
 
-gulp.task('prepublish', gulp.series('babel'));
-gulp.task('default', gulp.parallel('static', 'test'));
+//gulp.task('prepublish', ['babel']);
+
+function defaultTask(cb) {
+  cb();
+}
+
+exports.default = defaultTask
+function defaultTask(cb) {
+  // place code for your default task here
+  staticFiles();
+  test();
+  cb();
+}
+
+exports.build = series(
+  clean,
+  jsTranspile,
+  staticFiles,
+  testCoverage
+  //jsBundle,
+  //jsMinify,
+  //publish
+);
+
+exports.default = defaultTask
+
